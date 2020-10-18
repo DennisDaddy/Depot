280cd9281e1eeefc2850e663f51f3feb6cf13f3c
DennisDaddy <softteez95@gmail.com>
2020-10-18 07:48:02 +0300

    Add layout stylings and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,42 @@ Product.create!(title: 'Our love will never fade!!',
         </p>},
     image_url: '8apps.jpg',
     price: 26.00)
+
+    Product.create!(title: 'Our love will never fade!!',
+        description:
+            %{<p>
+            <em>Native Apps, Multiple Platforms</em>
+            Answer the question “Can we build this for ALL the devices?” with a
+            resounding YES. This book will help you get there with a real-world
+            introduction to seven platforms, whether you’re new to mobile or an
+            experienced developer needing to expand your options. Plus, you’ll find
+            out which cross-platform solution makes the most sense for your needs.
+            </p>},
+        image_url: '8apps.jpg',
+        price: 26.00)
+    
+    Product.create!(title: 'Our love will never fade!!',
+        description:
+            %{<p>
+            <em>Native Apps, Multiple Platforms</em>
+            Answer the question “Can we build this for ALL the devices?” with a
+            resounding YES. This book will help you get there with a real-world
+            introduction to seven platforms, whether you’re new to mobile or an
+            experienced developer needing to expand your options. Plus, you’ll find
+            out which cross-platform solution makes the most sense for your needs.
+            </p>},
+        image_url: '8apps.jpg',
+        price: 26.00)
+    
+    Product.create!(title: 'Our love will never fade!!',
+        description:
+            %{<p>
+            <em>Native Apps, Multiple Platforms</em>
+            Answer the question “Can we build this for ALL the devices?” with a
+            resounding YES. This book will help you get there with a real-world
+            introduction to seven platforms, whether you’re new to mobile or an
+            experienced developer needing to expand your options. Plus, you’ll find
+            out which cross-platform solution makes the most sense for your needs.
+            </p>},
+        image_url: '8apps.jpg',
+        price: 26.00)
