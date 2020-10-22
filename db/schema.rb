9990bbd0c623aefecf925ad828c10a832f344607
DennisDaddy <softteez95@gmail.com>
2020-10-22 06:34:33 +0300

    Migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_032323) do
+ActiveRecord::Schema.define(version: 2020_10_22_033301) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
